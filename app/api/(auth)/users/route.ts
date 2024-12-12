7cbca298c206a05deb60b6e15d919b9f27c21b2e
ronaldofreitas41 <ronaldinho.lfs@gmail.com>
2024-12-11 21:17:46 -0300

    adding Get route for Users API

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -1,10 +1,25 @@
 import { database } from "@/app/services/fbConfig";
-import { push, ref, set } from "firebase/database";
+import { child, get, push, ref, set } from "firebase/database";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-    return new NextResponse("This is my first Api response");
+    try {
+        const dbRef = ref(database);
+        const snapshot = await get(child(dbRef, 'Users'));
+
+        if (snapshot.exists()) {
+            return NextResponse.json({ data: snapshot.val() }, { status: 200 });
+        } else {
+            return NextResponse.json({ message: 'No data found' }, { status: 404 });
+        }
+    } catch (error: any) {
+        console.error('Error fetching data:', error.message);
+        return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+    }
+
 }
+
+
 
 export async function POST(request: Request) {
     try {
