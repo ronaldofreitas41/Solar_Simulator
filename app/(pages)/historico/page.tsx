d40b104d69b56521d0ed5db1addc1ca6def7526b
ronaldofreitas41 <ronaldinho.lfs@gmail.com>
2025-01-21 16:41:24 -0300

    Começando a criação da tela de histórico

---
+++ app/(pages)/historico/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+import NavBar from "@/app/components/Common/navBar";
+import YellowLine from "@/app/components/Common/yellowLine";
+import React from "react";
+
+export default function Home() {
+
+    return (
+        <div>
+            <NavBar usertype="none" />
+            <YellowLine />
+            
+
+        </div>
+    );
+}
