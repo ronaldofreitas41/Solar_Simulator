587d35eb2efd134e426f965e38fdc106a537146c
ronaldofreitas41 <ronaldinho.lfs@gmail.com>
2025-03-14 17:27:00 -0300

    Implement user authentication in POST requests, handle CORS preflight, and improve simulation data processing in the Simulator component

--- app/api/(simulacao)/irradiation/fotosinteticamenteAtiva/route.ts
+++ app/api/(simulacao)/irradiation/fotosinteticamenteAtiva/route.ts
@@ -21,17 +21,34 @@ export async function GET(request: NextRequest) {
 }
 
 export async function POST(request: NextRequest) {
- 
- 
     try {
-        const data = await request.json();
-        console.log('Data received:', data);    
-        const irradiacaoRef = push(ref(database, 'FotossinteticamenteAtiva'));
-        await set(irradiacaoRef, data);
+        // Se a requisição for OPTIONS (preflight CORS), retorne resposta vazia com status 200
+        if (request.method === "OPTIONS") {
+            return new NextResponse(null, { status: 200 });
+        }
 
-        return NextResponse.json({ message: 'Data saved successfully!' }, { status: 200 });
-    } catch (error:any) {
-        console.error('Error saving data:', error.message);
-        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+        // Processamento normal da requisição
+        const { email, password } = await request.json();
+        const dbRef = ref(database);
+        const snapshot = await get(child(dbRef, "FotossinteticamenteAtiva"));
+
+        if (snapshot.exists()) {
+            // Traz os usuários do banco para realizar a comparação
+            const users = snapshot.val();
+            const user = Object.values(users).find(
+                (user: any) => user.email === email && user.password === password
+            );
+
+            if (user) {
+                return NextResponse.json(user, { status: 200 });
+            } else {
+                return NextResponse.json({ message: "User does not exist" }, { status: 401 });
+            }
+        } else {
+            return NextResponse.json({ message: "No data found" }, { status: 404 });
+        }
+    } catch (error: any) {
+        console.error("Erro ao verificar usuário:", error.message);
+        return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
     }
 }
