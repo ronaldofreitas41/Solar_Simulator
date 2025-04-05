ecfc22cbfd45fed4b4f89f8d05ab8b9514f2918b
Ronaldo Luiz <65634807+ronaldofreitas41@users.noreply.github.com>
2025-04-05 17:23:49 -0300

    Refactor NavBar component to simplify Logout item rendering

--- app/components/Common/navBar.tsx
+++ app/components/Common/navBar.tsx
@@ -69,13 +69,7 @@ export const NavBar = () => {
                 {userType === 'Consumidor' && <HeaderItem id="Simular" text="Simular" href="/simular" />}
                 {userType === 'Fornecedor' && <HeaderItem id="Cadastrar" text="Cadastrar" href="/cadastro" />}
                 {!userType && <HeaderItem text="Login" href="/login" />}
-                {userType &&
-                    <HeaderItem
-                        href='/login'
-                        text="Logout"
-                        onClick={() => {
-                        }
-                        } />
+                {userType && <HeaderItem href='/login' text="Logout" onClick={() => { }} />
                 }
             </nav>
 
